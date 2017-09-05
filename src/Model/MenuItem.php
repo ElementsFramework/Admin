d0ccfa10f12c6ef6b54cc30ff5761971e858a1df
Igor Rinkovec <shiqqqi@gmail.com>
2017-09-05 12:39:08 +0200

    Added menu functinality.

--- src/Model/MenuItem.php
+++ src/Model/MenuItem.php
@@ -14,12 +14,12 @@ class MenuItem extends Model
 
     public function parent()
     {
-        return $this->belongsTo('Backpack\MenuCRUD\app\Models\MenuItem', 'parent_id');
+        return $this->belongsTo(MenuItem::class, 'parent_id');
     }
 
     public function children()
     {
-        return $this->hasMany('Backpack\MenuCRUD\app\Models\MenuItem', 'parent_id');
+        return $this->hasMany(MenuItem::class, 'parent_id');
     }
 
     public function page()
